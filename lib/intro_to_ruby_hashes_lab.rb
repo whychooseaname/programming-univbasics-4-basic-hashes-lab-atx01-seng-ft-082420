dbf38c7c14f92cb5ec0b74b826e6a4d1e56e13e7
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-05 04:17:19 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,16 @@ def new_hash
 end
 
 def my_hash
-  
+  {:me => "Eddie"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => "Grace Hopper"}
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  {
 end
